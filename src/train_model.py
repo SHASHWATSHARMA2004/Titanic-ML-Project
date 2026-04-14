fd0cedc3dd03bc69c6114aa23c0f3c85e3309612
Shashwat Sharma <shashwat@gmail.com>
2026-04-14 11:39:01 +0530

    Shashwat - Model Training

---
+++ src/train_model.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import joblib
+
+train = pd.read_csv("data/titanic.csv")
+
+X = train[["Pclass", "Sex", "Age", "Fare"]]
+y = train["Survived"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
+
+joblib.dump(model, "models/model.pkl")
